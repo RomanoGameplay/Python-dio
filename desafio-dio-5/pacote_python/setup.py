471c7bc8f7187408d3d4d7e9dbad9d08e2be7771
RomanoGameplay <82478812+RomanoGameplay@users.noreply.github.com>
2022-12-29 07:59:54 -0300

    Resolução de desafio de projeto

---
+++ desafio-dio-5/pacote_python/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='pacote_python',
+    version='0.0.1',
+    author='Romano',
+    author_email='caioprgonzaga@gmail.com',
+    description='exemplo de estrutura de um pacote python',
+    long_description='Este é um exmplo de criação e estrutura de um pacote python contendo módulos dentro deste',
+    url='https://github.com/RomanoGameplay/Python-dio/tree/main/desafio-dio-5',
+    packages=find_packages(),
+    python_requires='>=3.9'
+)
